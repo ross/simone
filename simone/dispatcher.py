53787a7b64d05c0bb0617be868d1ccf52f4ad7a4
Ross McFarland <rwmcfa1@gmail.com>
2021-10-11 20:05:00 -0700

    Implement in-memory rem command

--- simone/dispatcher.py
+++ simone/dispatcher.py
@@ -11,11 +11,57 @@ class Echo(object):
         context.say(text, channel, thread=thread)
 
 
+class Memory(object):
+    def __init__(self):
+        # TODO: this should be db backed
+        self.memory = {}
+
+    def config(self):
+        return {'commands': ('rem', 'remember', 'forget')}
+
+    def command(self, context, command, text, channel, thread, **kwargs):
+        print(f'\n\n{command}\n\n')
+        if command in ('rem', 'remember'):
+            if ' is ' in text:
+                # we're recording
+                what, about = text.split(' is ', 1)
+                self.memory[what] = about
+                context.say(
+                    f"OK. I'll remember {what} is {about}",
+                    channel,
+                    thread=thread,
+                )
+            elif text in self.memory:
+                context.say(
+                    f'{text} is {self.memory[text]}', channel, thread=thread
+                )
+            else:
+                context.say(
+                    f"Sorry. I don't remember anything about {text}",
+                    channel,
+                    thread=thread,
+                )
+        else:  # forget
+            if text in self.memory:
+                what = self.memory.pop(text)
+                context.say(
+                    f"OK. I'll forget that {text} was {what}",
+                    channel,
+                    thread=thread,
+                )
+            else:
+                context.say(
+                    f"Sorry. I don't remember anything about {text}",
+                    channel,
+                    thread=thread,
+                )
+
+
 class Dispatcher(object):
     def __init__(self):
         self.listeners = [SlackListener(self)]
 
-        handlers = [Echo()]
+        handlers = [Echo(), Memory()]
         self.handlers = handlers
         commands = {}
         for handler in handlers:
@@ -32,8 +78,9 @@ class Dispatcher(object):
         pprint({'type': 'added', 'args': args, 'kwargs': kwargs})
 
     def command(self, context, command, channel, *args, **kwargs):
+        # TODO: should context wrap up channel, thread etc?
         try:
-            command = self.commands[command]
+            handler = self.commands[command]
         except KeyError:
             # TODO: translation support?
             # TODO: respond private or in thread?
@@ -44,7 +91,9 @@ class Dispatcher(object):
             )
         else:
             # TODO: we should catch and log all exceptions down here
-            command.command(context, channel=channel, *args, **kwargs)
+            handler.command(
+                context, command=command, channel=channel, *args, **kwargs
+            )
 
     def edit(*args, **kwargs):
         pprint({'type': 'edit', 'args': args, 'kwargs': kwargs})
