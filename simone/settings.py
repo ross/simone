382084525d67b96c574ef560511fcc11a5c60098
Ross McFarland <rwmcfa1@gmail.com>
2021-10-16 18:50:31 -0700

    debug from an env var

--- simone/settings.py
+++ simone/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = environ['DJANGO_SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = environ.get('DJANGO_ALLOWED_HOSTS', '').split(';')
 
