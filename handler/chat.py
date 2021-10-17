dc8767932b2f7f00f50c6722bd9550b26673279d
Ross McFarland <rwmcfa1@gmail.com>
2021-10-16 17:36:46 -0700

    support help filtering

--- handler/chat.py
+++ handler/chat.py
@@ -45,6 +45,9 @@ class Help(object):
 
     To get the usage for a specific command:
       .help <command-name>
+
+    To filter commands to those that include a phrase in their name or help
+      .help | <word or phrase>
     '''
 
     def __init__(self):
@@ -61,36 +64,51 @@ class Help(object):
             text = f'{dispatcher.LEADER}{text}'
         except KeyError:
             try:
-                # try skipping a . in case they did `.help .foo`
+                # try skipping the leader in case they did `.help .foo`
                 handler = dispatcher.commands[text[len(dispatcher.LEADER) :]]
             except KeyError:
                 context.say(f'Sorry `{text}` is not a recognized command')
         context.say(f'Help for `{text}`\n```{handler.__doc__}```')
+
+    def _summarize_command(self, command, handler, dispatcher):
+        try:
+            short = handler.__doc__.split('\n')[1].strip()
+            maybe_short = f' - {short}'
+        except AttributeError:
+            maybe_short = ''
+        return f'  {dispatcher.LEADER}{command}{maybe_short}\n'
 
     def list_commands(self, context, dispatcher):
         if self._command_list is None:
             buf = StringIO()
             buf.write('Supported commands:\n```')
             for command, handler in sorted(dispatcher.commands.items()):
-                buf.write('  .')
-                buf.write(command)
-                try:
-                    short = handler.__doc__.split('\n')[1].strip()
-                    buf.write(' - ')
-                    buf.write(short)
-                except AttributeError:
-                    pass
-                buf.write('\n')
+                buf.write(self._summarize_command(command, handler, dispatcher))
             buf.write('```')
             self._command_list = buf.getvalue()
 
         context.say(self._command_list)
 
+    def search_commands(self, context, text, dispatcher):
+        text = text[1:].strip()
+        buf = StringIO()
+        buf.write('Commands matching `')
+        buf.write(text)
+        buf.write('`:\n```')
+        for command, handler in sorted(dispatcher.commands.items()):
+            if text in command or text in handler.__doc__:
+                buf.write(self._summarize_command(command, handler, dispatcher))
+                # we want to include this one
+        buf.write('```')
+        context.say(buf.getvalue())
+
     def command(self, context, text, dispatcher, **kwargs):
         if text:
-            self.help_command(context, text, dispatcher)
+            if text.startswith('|'):
+                self.search_commands(context, text, dispatcher)
+            else:
+                self.help_command(context, text, dispatcher)
         else:
-            # TODO: search/filter commands
             self.list_commands(context, dispatcher)
 
 
