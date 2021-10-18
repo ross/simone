a282b13fbc4d5db93cd4ab05fe28c43a505bddfe
Ross McFarland <rwmcfa1@gmail.com>
2021-10-18 14:57:02 -0700

    weather command

--- handler/chat.py
+++ handler/chat.py
@@ -1,4 +1,5 @@
 from io import StringIO
+from os import environ
 from requests import Session
 from time import sleep
 
@@ -216,3 +217,52 @@ class Wave(object):
 
 
 Registry.register_handler(Wave())
+
+
+class Weather(object):
+    '''
+    Get the current weather
+
+    To get the weather for a city
+      .weather <city name>
+    '''
+
+    URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+    def __init__(self, app_id):
+        self.app_id = app_id
+
+    def config(self):
+        return {'commands': ('weather',)}
+
+    def _weather_icon(self, data):
+        try:
+            weather = data['weather'][0]
+            icon = weather['icon']
+            desc = f'{weather["main"]} - {weather["description"]}'
+            return f'<https://openweathermap.org/img/wn/{icon}@2x.png|{desc}>'
+        except (IndexError, KeyError):
+            return ''
+
+    def command(self, context, text, **kwargs):
+        # TODO: support C?
+        params = {'q': text, 'appid': self.app_id, 'units': 'imperial'}
+        resp = session.get(self.URL, params=params)
+        if resp.status_code == 404:
+            # unknown location/city
+            context.say(f"Sorry. I wasn't able to find weather for `{text}`")
+            return
+        resp.raise_for_status()
+        data = resp.json()
+        main = data['main']
+        temp = main['temp']
+        feels_like = main['feels_like']
+        humidity = main['humidity']
+        weather_icon = self._weather_icon(data)
+        context.say(
+            f'Current weather for `{text}`: {temp}F, feels like {feels_like}F. {humidity}% humidity. {weather_icon}'
+        )
+
+
+app_id = environ['OPEN_WEATHER_MAP_APP_ID']
+Registry.register_handler(Weather(app_id))
