fa7f2368ea51b850ab0e5ac5d227f44703aa7245
Ross McFarland <rwmcfa1@gmail.com>
2021-10-17 17:39:01 -0700

    django settings as a dir

--- simone/settings/__init__.py
+++ simone/settings/__init__.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
