cfb5fce8036aee5c5a57acac841b17f869545908
Ross McFarland <rwmcfa1@gmail.com>
2021-10-17 18:25:03 -0700

    prod settings w/mysql support

---
+++ simone/settings/dev.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from os import environ
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s %(levelname)-5s %(name)s %(message)s',
+            'datefmt': '%Y-%m-%dT%H:%M:%SZ',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple',
+        },
+        'file': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'filename': 'django.log',
+        },
+    },
+    'root': {'level': 'DEBUG', 'handlers': ('console', 'file')},
+    'loggers': {
+        'django.db.backends': {
+            # comment out to see db queries
+            'level': 'INFO'
+        }
+    },
+}
---
+++ simone/settings/prod.py
@@ -0,0 +1,38 @@
+from os import environ
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': environ['SIMONE_DB_NAME'],
+        'USER': environ['SIMONE_DB_USER'],
+        'PASSWORD': environ['SIMONE_DB_PASSWORD'],
+        'HOST': environ['SIMONE_DB_HOST'],
+        'PORT': environ.get('SIMONE_DB_PORT', '3306'),
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s %(levelname)-5s %(name)s %(message)s',
+            'datefmt': '%Y-%m-%dT%H:%M:%SZ',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+            'formatter': 'simple',
+        },
+    },
+    'root': {'level': 'INFO', 'handlers': ('console',)},
+    'loggers': {
+        'django.db.backends': {
+            'level': 'INFO'
+        }
+    },
+}
