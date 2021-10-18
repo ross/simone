56ca66dad22bd70e4a3d8e327983e171b06a546d
Ross McFarland <rwmcfa1@gmail.com>
2021-10-18 08:35:48 -0700

    support for multi word commands

--- simone/dispatcher.py
+++ simone/dispatcher.py
@@ -19,14 +19,19 @@ class Dispatcher(object):
 
         messages = []
         commands = {}
+        multi_word_commands = {}
         for handler in handlers:
             config = handler.config()
             for command in config.get('commands', []):
-                commands[command] = handler
+                if ' ' in command:
+                    multi_word_commands[command] = handler
+                else:
+                    commands[command] = handler
             if config.get('messages', False):
                 messages.append(handler)
 
         self.commands = commands
+        self.multi_word_commands = multi_word_commands
         self.messages = messages
 
     def urlpatterns(self):
@@ -36,47 +41,58 @@ class Dispatcher(object):
     def added(*args, **kwargs):
         pprint({'type': 'added', 'args': args, 'kwargs': kwargs})
 
-    @transaction.atomic
-    def command(self, context, command, **kwargs):
-        try:
-            handler = self.commands[command]
-        except KeyError:
-            # score commands for "distant" from the command we received
-            commands = sorted(
-                [
-                    (levenshtein(command, name), name)
-                    for name, _ in self.commands.items()
-                ]
-            )
-            # only include things that are close enough, within 50% matches
-            cutoff = len(command) * 0.5
-            potentials = [
-                f'{self.LEADER}{name}'
-                for score, name in commands
-                if score < cutoff
+    def _did_you_mean(self, context, command):
+        # score commands for "distant" from the command we received
+        commands = sorted(
+            [
+                (levenshtein(command, name), name)
+                for name, _ in self.commands.items()
             ]
-            # build the response
-            buf = StringIO()
-            buf.write('Sorry `')
-            buf.write(command)
-            buf.write('` is not a recognized command.')
+        )
+        # only include things that are close enough, within 50% matches
+        cutoff = len(command) * 0.5
+        potentials = [
+            f'{self.LEADER}{name}' for score, name in commands if score < cutoff
+        ]
+        # build the response
+        buf = StringIO()
+        buf.write('Sorry `')
+        buf.write(command)
+        buf.write('` is not a recognized command.')
 
-            if potentials:
-                buf.write(" Maybe you're looking for `")
+        if potentials:
+            buf.write(" Maybe you're looking for `")
 
-                last = potentials.pop()
-                n = len(potentials)
-                if n > 1:
-                    buf.write('`, `'.join(potentials))
-                    buf.write('`, or `')
-                elif n == 1:
-                    buf.write(potentials[0])
-                    buf.write('` or `')
+            last = potentials.pop()
+            n = len(potentials)
+            if n > 1:
+                buf.write('`, `'.join(potentials))
+                buf.write('`, or `')
+            elif n == 1:
+                buf.write(potentials[0])
+                buf.write('` or `')
                 buf.write(last)
 
                 buf.write('`.')
 
             context.say(buf.getvalue())
+
+    @transaction.atomic
+    def command(self, context, command, text, **kwargs):
+        try:
+            # look for an exact match single word command
+            handler = self.commands[command]
+        except KeyError:
+            # TODO: this should probably do the splitting/parsing
+            command = f'{command} {text}'
+            for name, handler in self.multi_word_commands.items():
+                if command.startswith(name):
+                    text = command.split(name, 1)[1]
+                    command = name
+                    break
+
+        if not handler:
+            self._did_you_mean(context, command)
         else:
             try:
                 handler.command(
@@ -104,6 +120,7 @@ class Dispatcher(object):
     def message(self, *args, **kwargs):
         pprint({'type': 'left', 'args': args, 'kwargs': kwargs})
         for handler in self.messages:
+            # TODO: these should all wrap with error handling
             handler.message(*args, **kwargs)
 
     @transaction.atomic
